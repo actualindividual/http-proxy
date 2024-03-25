a5df4522eddb0020a55e085c62a5e1abe3bb912e
e <thehumanej@gmail.com>
2024-03-25 15:57:52 -0500

    create new branch for working with a better proxy library

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-const express = require('express');
-const morgan = require("morgan");
-const { createProxyMiddleware } = require('http-proxy-middleware');
-require('dotenv').config()
+var http = require('http'),
+    httpProxy = require('http-proxy');
+//require('dotenv');
 
-// Creating express server
-const app = express();
+//let api_url = process.env.API_URL;
 
-const PORT = process.env.PORT;
-const HOST = "localhost";
-const API_URL = process.env.API_URL;
-//const API_KEY_VALUE = process.env.API_KEY_VALUE;
-
-app.use('/matchinput', createProxyMiddleware({
-    target: API_URL,
-    changeOrigin: true,
-    pathRewrite: {
-        [`^/matchinput`]: '',
-    },
-}));
-
-app.listen(PORT, HOST, () => { 
-    console.log(`Starting Proxy at ${HOST}:${PORT}`); 
-});
+httpProxy.createProxyServer({target:'http://98.59.100.219:3082/matchinput'}).listen(443);
